fda8899077dd80787447fad40b12d57cd7801deb
korayerdem89 <developerkoray@gmail.com>
2022-07-20 02:04:58 +0300

    JobsCard component dosyası oluşturuldu

---
+++ src/Components/JobsCard/JobsCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+const JobsCard = () => {
+return (
+    <View>
+        <Text>Hello Moto</Text>
+    </View>
+)
+}
+
+export default JobsCard;
+
--- src/Pages/Jobs/Jobs.js
+++ src/Pages/Jobs/Jobs.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, ActivityIndicator } from 'react-native';
 import styles from './Jobs.style';
 import config from "../../../config";
 import useFetch from "../../hooks/useFetch/useFetch";
+import JobsCard from '../../Components/JobsCard/JobsCard';
 const Jobs = () => {
     const { data, loading, error } = useFetch(`${config.API_JOBS_URL}`);
- 
+
+    if (loading) {
+        return <ActivityIndicator size="large" />
+    }
+
+    if (error) {
+        return <Text>{error}</Text>;
+    }
+
     return (
         <SafeAreaView>
-            <Text>Hello</Text>
+            <JobsCard />
         </SafeAreaView>
     );
 };
