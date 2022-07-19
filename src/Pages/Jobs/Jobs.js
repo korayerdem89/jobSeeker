319e6cc5bccfdf95d7dba6fe153bbb0f2df7a254
korayerdem89 <developerkoray@gmail.com>
2022-07-20 00:36:57 +0300

    sayfalar ve iskeletleri oluşturuldu

---
+++ src/Pages/Jobs/Jobs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+import styles from './Jobs.style';
+
+const Jobs = () => {
+
+
+    return (
+        <SafeAreaView>
+            <Text>Hello</Text>
+        </SafeAreaView>
+    );
+};
+
+
+export default Jobs;
