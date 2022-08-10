559a17c4f82c95d010bea6df237fd2e389ed7c41
korayerdem89 <developerkoray@gmail.com>
2022-08-11 02:46:15 +0300

    kod içinde açıklamalar eklendi

--- src/Pages/Jobs/Jobs.js
+++ src/Pages/Jobs/Jobs.js
@@ -10,8 +10,8 @@ import { nextPage, previousPage } from '../../context/JobsSlice';
 const Jobs = ({ navigation }) => {
     const page = useSelector(state => state.jobs.pageNumber);
     const [pageNumber, setPageNumber] = useState(page);
- 
-    const { data, loading, error } = useFetch(`${config.API_URL}?page=${pageNumber}`, pageNumber);
+
+    const { data, loading, error } = useFetch(`${config.API_URL}?page=${pageNumber}`, pageNumber); //dinamik sayfa değişimi için useFecth hookuna "pageNumber" değişkeni gönderildi, böylece hooks içindeki useEffect, her sayfa değişiminde tetiklenerek yeni sayfaya geçiyor. (sayfa değiştirmek için redux'ı tercih etmemin sebebi pekiştirme amaçlıdır, daha kolay yöntemler de mevcut"
     const dispatch = useDispatch();
     if (loading) {
         return <ActivityIndicator size="large" />
@@ -30,13 +30,13 @@ const Jobs = ({ navigation }) => {
     )
 
     const handleNextPage = () => {
-        dispatch(nextPage()); 
-        setPageNumber(page+1);
+        dispatch(nextPage());
+        setPageNumber(page + 1); //setPageNumber(page) deseydim, hooks içindeki useEffect, bir önceki pageNumber değerini baz alarak tetiklenirdi, bu da istenmeyen bir durumdur"
     }
 
     const handlePreviousPage = () => {
         dispatch(previousPage());
-        setPageNumber(page-1)
+        setPageNumber(page - 1) //setPageNumber(page) deseydim, hooks içindeki useEffect, bir önceki pageNumber değerini baz alarak tetiklenirdi, bu da istenmeyen bir durumdur"
     }
 
     return (
