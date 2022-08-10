7788b49a3606200bfab60c568328374a112bcda8
korayerdem89 <developerkoray@gmail.com>
2022-08-11 00:03:01 +0300

    proje sonu

--- src/context/JobsSlice.js
+++ src/context/JobsSlice.js
@@ -1,33 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const jobsSlice=createSlice({
-  name:'jobs',
-  initialState:{
-    favoritedJobs:[],
-    submittedJobs:[],
+export const jobsSlice = createSlice({
+  name: 'jobs',
+  initialState: {
+    favoritedJobs: [],
+    submittedJobs: [],
   },
-  reducers:{
-    addFavorite:(state,action)=>{
+  reducers: {
+    addFavorite: (state, action) => {
       let favList = [];
-      const item =action.payload;
-      favList =[...state.favoritedJobs, item];
-      return {...state, favoritedJobs:favList}
+      const item = action.payload;
+      favList = [...state.favoritedJobs, item];
+      return { ...state, favoritedJobs: favList }
     },
-    addSubmitted:(state,action)=>{
+    addSubmitted: (state, action) => {
       let submitList = [];
-      const item =action.payload;
+      const item = action.payload;
       submitList = [...state.submittedJobs, item];
-      return {...state, submittedJobs:submitList}
+      return { ...state, submittedJobs: submitList }
     },
-    removeJob:(state,action)=>{
-      const id=action.payload;
-      const newArr=state.favoritedJobs.filter(item=> item.id !== id);
-      return {...state, favoritedJobs:newArr}
+    removeJob: (state, action) => {
+      const id = action.payload;
+      const newArr = state.favoritedJobs.filter(item => item.id !== id);
+      return { ...state, favoritedJobs: newArr }
     },
   }
 });
 
 
 
-export const {addFavorite, removeJob, addSubmitted} = jobsSlice.actions;
+export const { addFavorite, removeJob, addSubmitted } = jobsSlice.actions;
 export default jobsSlice.reducer;
